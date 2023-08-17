2f544748a43300cda1486ea69d26b326fe27037f
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-08-17 16:55:12 +0200

    fix light settings bug

--- app/src/main/java/com/project/mobile_project/MainActivity.kt
+++ app/src/main/java/com/project/mobile_project/MainActivity.kt
@@ -3,23 +3,32 @@ package com.project.mobile_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.project.mobile_project.ui.theme.Mobile_projectTheme
+import com.project.mobile_project.viewModel.SettingsViewModel
 import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val settingsViewModel: SettingsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Mobile_projectTheme {
+            val theme by settingsViewModel.theme.collectAsState(initial = "")
+            Mobile_projectTheme(darkTheme = theme == "Dark") {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
