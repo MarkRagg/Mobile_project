6b2f53b74e4f0330cbd442ad2df19f497bd234b4
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2023-08-19 19:24:11 +0200

    implement logout

--- app/src/main/java/com/project/mobile_project/TravelApp.kt
+++ app/src/main/java/com/project/mobile_project/TravelApp.kt
@@ -1,6 +1,11 @@
 package com.project.mobile_project
 
 import android.app.Application
+import android.app.PendingIntent.getActivity
+import android.app.SharedElementCallback
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -10,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.core.content.ContextCompat.startActivity
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -23,6 +29,7 @@ import com.project.mobile_project.ui.ProfileScreen
 import com.project.mobile_project.ui.SettingsScreen
 import com.project.mobile_project.viewModel.SettingsViewModel
 import dagger.hilt.android.HiltAndroidApp
+import java.security.AccessController.getContext
 
 @HiltAndroidApp
 class TravelApp : Application() {
@@ -124,7 +131,9 @@ fun BottomAppBarFunction(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavigationApp(
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController = rememberNavController(),
+    sharedPreferences: SharedPreferences,
+    context: Context
 ) {
     // Get current back stack entry
     val backStackEntry by navController.currentBackStackEntryAsState()
@@ -138,7 +147,7 @@ fun NavigationApp(
                 canNavigateBack = navController.previousBackStackEntry != null,
                 navigateUp = { navController.navigateUp() },
                 onSettingsButtonClicked = {navController.navigate(AppScreen.Settings.name)},
-                onLogoutButtonClicked = {navController.navigate(AppScreen.Login.name)}
+                onLogoutButtonClicked = {logout(sharedPreferences, context)}
             )
         },
         bottomBar = {
@@ -188,4 +197,16 @@ private fun NavigationGraph(
             SettingsScreen(settingsViewModel)
         }
     }
+}
+
+private fun logout(sharedPreferences: SharedPreferences, context: Context) {
+    val editor: SharedPreferences.Editor = sharedPreferences.edit()
+    val loginIntent = Intent(context, LoginScreen::class.java)
+    loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+    editor.putBoolean("userLogged", false)
+    editor.putString("usernameLogged", "")
+    editor.apply()
+
+    startActivity(context, loginIntent, null)
 }
